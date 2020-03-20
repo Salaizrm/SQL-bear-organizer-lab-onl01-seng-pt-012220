8d116e0b3499c69286a3d02f97af6b9eb57b3ff5
Robert Salaiz <work.salaizrm@gmail.com>
2020-03-20 17:51:01 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,8 +1,8 @@
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",10,"Male","brown","nice",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",15,"Male","black","tired",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",10,"Male","brown","nice",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Seargent Brown",10,"Male","brown","nice",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",7,"Female","brown","mischievous",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",30,"Male","brown","mean",1);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",10,"Male","brown","nice",0);
-INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,25,"Male","brown","unknown",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Mr.Chocolate",10,"male","brown","nice",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Rowdy",15,"male","black","tired",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Tabitha",10,"male","brown","nice",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Seargent Brown",10,"male","brown","nice",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Melissa",7,"female","brown","mischievous",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Grinch",30,"male","brown","mean",1);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES ("Wendy",10,"female","brown","nice",0);
+INSERT INTO bears (name,age,gender,color,temperament,alive) VALUES (NULL,25,"male","brown","unknown",1);
